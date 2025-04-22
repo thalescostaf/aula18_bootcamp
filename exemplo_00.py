71dd14973a2aa16922c3c99515de9ae53f1ad83b
Thales Fernandes <contato.thales@gmail.com>
2025-04-22 19:58:23 -0300

    Instalação do Pydantic e exemplo de contrato de dados

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,13 +1,25 @@
 import requests
+from pydantic import BaseModel
 
-response = requests.get(f'https://pokeapi.co/api/v2/pokemon/15')
-data = response.json()
-data_types = data['types']
-type_list =[]
-for type_info in data_types:
-    type_list.append(type_info['type']['name'])
-types = ', '.join(type_list)
-print(data['name'],types)
+class PokemonSchema(BaseModel):
+    name: str
+    type: str
 
+    class Config:
+        orm_mode: True
 
+def pegar_pokemon(id: int) -> PokemonSchema:
+    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{id}')
+    data = response.json()
+    data_types = data['types']
+    type_list =[]
+    for type_info in data_types:
+        type_list.append(type_info['type']['name'])
+    types = ', '.join(type_list)
+    return PokemonSchema(name=data['name'], type=types)
+
+if __name__ == '__main__':
+    print(pegar_pokemon(10))
+    print(pegar_pokemon(6))
+    print(pegar_pokemon(13))
 
